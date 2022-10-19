c0006b94ff359a020651b66f1850b0a3f5a3b990
NovozhilovAY <sasha.n191@yandex.ru>
2022-10-19 17:10:04 +0300

    fix checkstyle

--- core/src/main/java/liga/medical/messageanalyzer/core/config/RabbitConfig.java
+++ core/src/main/java/liga/medical/messageanalyzer/core/config/RabbitConfig.java
@@ -9,23 +9,27 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
 @Configuration
 public class RabbitConfig {
-    private static final String HOST = "localhost";
+
     public static final String QUEUE_NAME = "common_monitoring";
+    private static final String HOST = "localhost";
+
     @Bean
     public ConnectionFactory connectionFactory() {
         return new CachingConnectionFactory(HOST);
     }
+
     @Bean
     public AmqpAdmin amqpAdmin() {
         return new RabbitAdmin(connectionFactory());
     }
+
     @Bean
     public RabbitTemplate rabbitTemplate() {
         return new RabbitTemplate(connectionFactory());
     }
+
     @Bean
     public Queue getQueue() {
         return new Queue(QUEUE_NAME);
